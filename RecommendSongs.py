cb962d9fc47195561ce42597524bb81a05eed10b
KyleWynne <69371846+KyleWynne@users.noreply.github.com>
2023-04-20 18:31:12 -0700

    Add files via upload

--- RecommendSongs.py
+++ RecommendSongs.py
@@ -1,3 +1,8 @@
+import sqlite3
+from database import db_operations
+
+db_ops = db_operations("SongData.db")
+
 def recommendSong(songs): #returns a list of song ID's to be presented to User  
   #set query values
   query_value = 0.10
@@ -6,13 +11,47 @@ def recommendSong(songs): #returns a list of song ID's to be presented to User
   curr_list = []
   prev_list = []
 
+  danceability = track_audio["danceability"]
+  energy = track_audio["energy"]
+  #loudness = str(track_audio["loudness"])
+  speechiness = track_audio["speechiness"]
+  acousticness = track_audio["acousticness"]
+  valence = track_audio['valence']
+  #tempo = str(track_audio['tempo'])
+  Instrumentalness = track_audio['instrumentalness']
+
   #run initial query based off Energy
 
   #enters while loop (list is not equal to 5)
   while(len(curr) != 5):
 
     #query based off of curr feature and value
+    energy_featL = str(energy - query_value)
+    energy_featH = str(energy + query_value)
+
+    danceability_featL = str(danceability - query_value)
+    danceability_featH = str(danceability + query_value)
+
+    speechiness_featL = str(speechiness - query_value)
+    speechiness_featH = str(speechiness + query_value)
+
+    acousticness_featL = str(acousticness - query_value)
+    acousticness_featH = str(acousticness + query_value)
+
+    valence_featL = str(valence - query_value)
+    valence_featH = str(valence + query_value)
+
+    Instrumentalness_featL = str(Instrumentalness - query_value)
+    Instrumentalness_featH = str(Instrumentalness + query_value)
+
+    query = '''
+    SELECT SongID
+    FROM Song_Data
+    WHERE Energy >='''+energy_featL+" AND Energy<="+energy_featH+" AND Danceability >="+danceability_featL+" AND Danceability <="+danceability_featH+'''
+    AND Speechiness >='''+speechiness_featL+" AND Speechiness <="+speechiness_featH+" AND Acousticness >="+acousticness_featL+" AND Acousticness <="+acousticness_featH+'''
+    AND Valence >='''+valence_featL+" AND Valence <="+valence_featH+" AND Instrumentalness >="+Instrumentalness_featL+" AND Instrumentalness <="+Instrumentalness_featH+";"
     #set curr to query
+    curr = db_ops.execute_one(query)
 
     #if length equals 5 exactly
     if(len(curr) == 5): #CASE A. List is perfectly 5, break
@@ -61,7 +100,6 @@ def recommendSong(songs): #returns a list of song ID's to be presented to User
       #break
       break
 
-  #Add 5 random songs onto curr
   #return CURR
   return curr
 
