dec11d619a055e6776519170a3a2f43f00a055e2
KyleWynne <69371846+KyleWynne@users.noreply.github.com>
2023-04-25 17:39:17 -0700

    Add files via upload

--- fetch_song_data.py
+++ fetch_song_data.py
@@ -74,5 +74,68 @@ valence = str(track_audio['valence'])
 tempo = str(track_audio['tempo'])
 Instrumentalness = str(track_audio['instrumentalness'])
 
-query = "INSERT INTO Song_Data VALUES(\'"+song_id+"\', \'"+name+"\', \'"+artist+"\', "+danceability+", "+energy+","+key+","+loudness+","+mode+","+speechiness+","+acousticness+","+Instrumentalness+","+liveness+","+valence+","+tempo+",\'"+track_link+"\');"
-db_ops.execute_one(query)
+name = name.replace('\'', 'º')
+artist = artist.replace('\'', 'º')
+
+query = '''
+SELECT *
+FROM Song_Data
+WHERE SongID = \''''+song_id+"\';"
+
+test = db_ops.execute_one(query)
+
+if not test:
+    query = "INSERT INTO Song_Data VALUES(\'"+song_id+"\', \'"+name+"\', \'"+artist+"\', "+danceability+", "+energy+","+key+","+loudness+","+mode+","+speechiness+","+acousticness+","+Instrumentalness+","+liveness+","+valence+","+tempo+",\'"+track_link+"\');"
+    db_ops.execute_one(query)
+else:
+    print("song already in db")
+
+danceability = track_audio["danceability"]
+energy = track_audio["energy"]
+#loudness = str(track_audio["loudness"])
+speechiness = track_audio["speechiness"]
+acousticness = track_audio["acousticness"]
+valence = track_audio['valence']
+#tempo = str(track_audio['tempo'])
+Instrumentalness = track_audio['instrumentalness']
+
+query_value = 0.05
+
+#query based off of curr feature and value
+
+while 1:
+
+    energy_featL = str(energy - query_value)
+    energy_featH = str(energy + query_value)
+
+    danceability_featL = str(danceability - query_value)
+    danceability_featH = str(danceability + query_value)
+
+    speechiness_featL = str(speechiness - query_value)
+    speechiness_featH = str(speechiness + query_value)
+
+    acousticness_featL = str(acousticness - query_value)
+    acousticness_featH = str(acousticness + query_value)
+
+    valence_featL = str(valence - query_value)
+    valence_featH = str(valence + query_value)
+
+    Instrumentalness_featL = str(Instrumentalness - query_value)
+    Instrumentalness_featH = str(Instrumentalness + query_value)
+
+    query = '''
+    SELECT Name
+    FROM Song_Data
+    WHERE Energy >='''+energy_featL+" AND Energy<="+energy_featH+" AND Danceability >="+danceability_featL+" AND Danceability <="+danceability_featH+'''
+    AND Speechiness >='''+speechiness_featL+" AND Speechiness <="+speechiness_featH+" AND Acousticness >="+acousticness_featL+" AND Acousticness <="+acousticness_featH+'''
+    AND Valence >='''+valence_featL+" AND Valence <="+valence_featH+" AND Instrumentalness >="+Instrumentalness_featL+" AND Instrumentalness <="+Instrumentalness_featH+" LIMIT 6;"
+    #set curr to query
+    curr = db_ops.execute_one(query)
+    print(curr)
+
+    if len(curr) == 6:
+        break
+    elif len(curr) < 6:
+        query_value +=0.01
+
+    print(curr)
