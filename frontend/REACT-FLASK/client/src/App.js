afc21554daf4b3d6df8a11eca2bbe2fa29efa4b3
Lauren Szlosek <szlosek@chapman.edu>
2023-05-03 15:35:39 -0700

    dk why it's 948 changes lol

--- frontend/REACT-FLASK/client/src/App.js
+++ frontend/REACT-FLASK/client/src/App.js
@@ -1,26 +1,50 @@
+// https://www.youtube.com/watch?v=EcRFYF4B3IQ
 import React, { useState, useEffect} from 'react'
 
 function App() {
-
-  const [data, setData] = useState([{}])
+    const [userInput, setUserInput] = useState('')
+    const [output, setOutput] = useState([{}])
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const submittedSong = {userInput}
+        console.log({submittedSong})
+    }
 
   useEffect( () => {
         fetch("/songs").then(
             res => res.json()
         ).then(
-            data => {
-                setData(data)
-                console.log(data)
+            output => {
+                setOutput(output)
+                console.log(output)
             }
         )
     }, [])
 
   return (
+    //   
         <div>
-            {(typeof data.songs === 'undefined') ? (
+
+
+        <h1 align="center">Tired of your current playlist?</h1>
+        <p align="center">[      ]</p>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    required
+                    value={userInput}
+                    onChange={(e) => setUserInput(e.target.value)}
+                />
+            </form>
+            {/* <p>{userInput}</p> */}
+
+
+
+            {(typeof output.songs === 'undefined') ? (
                 <p>Loading...</p>
             ) : (
-                data.songs.map((songs, i) => (
+                output.songs.map((songs, i) => (
                     <p key={i}>{songs}</p>
                 ))
             )}
