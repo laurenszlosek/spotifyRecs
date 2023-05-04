09d3436239b287348dbdbdf7c908b7464bc03ad7
Lauren Szlosek <szlosek@chapman.edu>
2023-05-03 22:41:28 -0700

    more frontend updates

--- frontend/app.py
+++ frontend/app.py
@@ -1,18 +1,19 @@
 # to run- in terminal:
 # python3 -m venv venv
 # source venv/bin/activate
+# (only once) pip3 install flask
+# flask run
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    output_lines = []
     if request.method == 'POST':
         user_input = request.form['user_input']
         with open('user_input.txt', 'w') as f:
             f.write(user_input)
-        output_lines = get_output_lines()
-    else:
         output_lines = get_output_lines()
     return render_template('index.html', output_lines=output_lines)
 
