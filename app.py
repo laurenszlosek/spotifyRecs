ae99d7b985164fd944c1e9b075e4ea4fea21c12f
Lauren Szlosek <69598004+laurenszlosek@users.noreply.github.com>
2023-05-09 18:55:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
-# to run- in terminal:
-# cd frontend
-# python3 -m venv venv
-# source venv/bin/activate
-# (only once) pip3 install flask
-# flask run or python3 -m flask run
 from flask import Flask, render_template, request
-# from main import Backend
-
+from main import Backend
+import subprocess
 app = Flask(__name__)
-# FC = Backend()
-
+backend = Backend()
 @app.route('/', methods=['GET', 'POST'])
 def index():
     output_lines = []
@@ -17,7 +10,7 @@ def index():
         user_input = request.form['user_input']
         with open('user_input.txt', 'w') as f:
             f.write(user_input)
-            # FC.main()
+        run_main()
         output_lines = get_output_lines()
     return render_template('index.html', output_lines=output_lines)
 
@@ -25,6 +18,9 @@ def get_output_lines():
     with open('output.txt', 'r') as f:
         output_lines = f.readlines()
     return output_lines
+
+def run_main():
+    subprocess.check_output(['python', '-c', 'from main import Backend; Backend().main()'])
 
 if __name__ == '__main__':
     app.run(debug=True)
